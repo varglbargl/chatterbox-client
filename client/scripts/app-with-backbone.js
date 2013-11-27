a35b0dbe551a2900cfdc2817a7b50fdf7af84e04
Eric <ehannum@gmail.com>
2013-11-26 16:13:34 -0800

    More backbone conversion work.

--- client/scripts/app-with-backbone.js
+++ client/scripts/app-with-backbone.js
@@ -3,26 +3,37 @@
 var lastId = "";
 var chatRooms = [];
 var currentRoom = "all";
-var security = new RegExp("[a-zA-Z0-9_,\s \=\.\@\#\?\!\:\(\)\'\-]*");
-var events = _.clone(Backbone.Events);
+var security = new RegExp("[a-zA-Z0-9_,\s \=\.\@\#\?\!\:\(\)\'\"\-]*");
+var username;
 
-var Message = function () {
-  this.soup = "delicious";
+var getUsername = function(variable){
+  var query = window.location.search;
+  var vars = query.split("=");
+  username = vars[vars.length-1];
 };
 
-Message.prototype.send = function (input) {
-  $.ajax({
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: "POST",
-    contentType: 'application/json',
-    dataType: "json",
-    data: JSON.stringify({username: input.username, text: input.text, roomname: input.roomname }),
-    success: input.success,
-    error: input.error
-   });
-};
+var Message = Backbone.Model.extend({
+  url: 'https://api.parse.com/1/classes/chatterbox'
+});
 
-var fullGet = function(){
+var Messages = Backbone.Collection.extend({
+  // send: function (input) {
+  //   var that = this;
+  //   var message = new Message();
+  //   message.save(input, {
+  //     success: function (e) {
+  //       that.trigger("message:send");
+  //       console.log('chatterbox: Message sent');
+  //     },
+  //     error: function (e) {
+  //       console.error('chatterbox: Failed to send message\nERROR: ' + e);
+  //     }
+  //   });
+  // }
+  model: Message
+});
+
+var fullFetch = function(){
   $.get( "https://api.parse.com/1/classes/chatterbox?order=-createdAt", function(data) {
     for (var i = 0; i < data.results.length; i++) {
       var text = security.exec(data.results[i].text);
@@ -38,10 +49,8 @@ var fullGet = function(){
           chatRooms.push(room);
           $(".roomname").on('click', function () {
             currentRoom = $(this).text();
-            //console.log(currentRoom);
             $(".message").remove();
             $(".chatbox").prepend("<span class=\"message server\"><b> Switched to room " + currentRoom + "</b></span><br class=\"message\" />");
-            //fullGet();
           });
         }
       }
@@ -76,46 +85,38 @@ var fetch = function () {
   });
 };
 
-var NewMessageView = function (options) {
-  this.message = options.message;
+var NewMessageView = Backbone.View.extend({
+  initialize: function () {
 
-  events.on("message:send", this.updateChat, this);
-  events.on("message:send", this.clearInput, this);
+    this.collection.on("add", this.updateChat, this);
+    this.collection.on("add", this.clearInput, this);
 
-  var add = $.proxy(this.sendMessage, this);
-  $(".submit").click(add);
-}
-
-NewMessageView.prototype.sendMessage = function () {
-  this.message.send({
-    username: "A Backbone Tester",
-    text: $(".textbox").val(),
-    roomname: currentRoom,
-    success: function () {
-      events.trigger("message:send");
-      console.log('chatterbox: Message sent');
-    },
-    error: function () {
-      console.error('chatterbox: Failed to send message');
-    }
-  });
-}
-
-NewMessageView.prototype.clearInput = function () {
-  $(".textbox").val("");
-}
-
-NewMessageView.prototype.updateChat = function () {
-  fetch();
-}
+    var add = $.proxy(this.sendMessage, this);
+    this.$("button").click(add);
+  },
+  sendMessage: function () {
+    this.collection.create({
+      username: username,
+      text: this.$("input").val(),
+      roomname: currentRoom
+    });
+  },
+  updateChat: function () {
+    fetch();
+  },
+  clearInput: function () {
+    this.$("input").val("");
+  }
+});
 
 $(document).ready(function (){
 
   setInterval(fetch, 1000);
-  fullGet();
+  fullFetch();
+  getUsername();
 
-  var message = new Message();
-  new NewMessageView({message: message});
+  var message = new Messages();
+  new NewMessageView({el: $(".messagebox"), collection: message});
 
 
 
@@ -128,7 +129,7 @@ $(document).ready(function (){
   // shoutbot
 
   var yo = ["What's up", "Keep walking", "Go to sleep", "Never change", "You're a star", "Have some of my fries", "Keep up the pace", "Congratulations", "How's it going", "Get a job", "Keep it real", "You can do it", "We're all very proud of you", "See ya later", "Give 'em hell", "You're my number one customer", "Outa my way"];
-  var clam = ["Chuggin'", "Super", "Diaper", "Tons a", "Double", "Burger", "Danger", "Bunch a", "Hot", "Helicopter", "Horse", "Tiny", "Butter", "Slappy", "Dunder", "Slurpee", "Tuba", "Soup", "Dungeon", "Power", "Shrimp", "Star", "Lumpy", "Laundry", "Frowny", "Electro", "Flamingo", "Lumber", "Frumpy", "Monster", "Tiger", "Moon", "Oyster", "Clam", "Thunder", "Blunder", "Onion", "Chowder", "Shakey", "Math", "Dino", "Disco", "Jazz", "Motor", "Junior", "Piano", "Potato"];
+  var clam = ["Chuggin'", "Super", "Double", "Burger", "Danger", "Bunch a", "Hot", "Helicopter", "Horse", "Tiny", "Butter", "Slappy", "Dunder", "Slurpee", "Tuba", "Soup", "Dungeon", "Power", "Shrimp", "Star", "Lumpy", "Laundry", "Frowny", "Electro", "Flamingo", "Lumber", "Frumpy", "Monster", "Tiger", "Moon", "Oyster", "Clam", "Thunder", "Blunder", "Onion", "Chowder", "Shakey", "Math", "Dino", "Disco", "Jazz", "Motor", "Junior", "Piano", "Potato"];
   var hands = ["Tubs", "Patrol", "Wagon", "Dump", "Fun", "Stump", "Dooper", "Time", "Mode", "Zone", "Stamps", "Chunks", "Helmet", "Nuggets", "Pilot", "Boots", "Legs", "Guy", "Barrel", "Batter", "Shanks", "Phone", "Police", "Socks", "Bumbler", "Peepers", "Bags", "Spray", "Clown", "Shorts", "Goggles", "Pile", "Farm", "Dome", "Dude", "Jams", "Town", "Mash", "Mouth from Smash Mouth", "Wads", "Hugs", "Buns", "Boats", "Style", "Shades", "Store", "Guns", "Polish", "Mash", "Scoops", "Town", "Hands", "Rolls", "Puncher", "Gums", "House", "Blaster", "Damage", "Repellent", "Roller", "Stack", "Cannon"];
 
   var spam = function () {
@@ -149,8 +150,8 @@ $(document).ready(function (){
       }
     });
 
-    setTimeout(function() { spam(); }, Math.random() * 50000 + 60000);
+    setTimeout(function() { spam(); }, Math.random() * 120000 + 120000);
   }
 
-  setTimeout(function() { spam(); }, Math.random() * 50000 + 60000);
+  setTimeout(function() { spam(); }, Math.random() * 60000 + 120000);
 });
